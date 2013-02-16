4f3e212eae9c2d7373f711601c229f5e3f32c16e
Kevin Patton <kevinpatton@gmail.com>
2013-02-15 20:08:40 -0600

    Fixed a performance issue. Previously, the main menu's background image was being loaded every single frame! No longer!

--- Tetris/src/javagame/Menu.java
+++ Tetris/src/javagame/Menu.java
@@ -1,46 +1,66 @@
 package javagame;
 
-//import org.lwjgl.input.Mouse;
-import org.newdawn.slick.*;
-import org.newdawn.slick.state.*;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.UnicodeFont;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
 
+/**
+ * @author Kevin Patton
+ * This class represents the main menu state of the game.
+ */
 public class Menu extends BasicGameState {
-	
-	//private String mousePositionInfo = "No input yet!";
-	//private Image gordon;
-	//private int gordonX = 200;
-	//private int gordonY = 200;
+	/** the font size for the title of the game */
 	public static final int FONT_SIZE = 65;
 	
-	public MenuStrip menu;
+	public  MenuStrip   menu;
 	private UnicodeFont titleFont;
-	private Play play;
+	private Image       bg;
+	private Play        play;
+	private int         state;
 	
 	public Menu(int state) {
+		this.state = state;
 	}
 	
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-		
-		titleFont = Utility.getFont(FONT_SIZE);
-		
+		//build the MenuStrip
 		int y = 0;
 		Interactable start = new MenuItemString("New Game", 0, y);
 		int lineHeight = start.getHeight();
-		Interactable options = new MenuItemString("Options", 0, y += lineHeight);
-		Interactable credits = new MenuItemString("Credits", 0, y += lineHeight);
-		Interactable quit = new MenuItemString("Quit", 0, y += lineHeight);
+		Interactable options = new MenuItemString("Options", 0, y += 
+				lineHeight);
+		Interactable credits = new MenuItemString("Credits", 0, y +=
+				lineHeight);
+		Interactable quit = new MenuItemString("Quit", 0, y +=
+				lineHeight);
+		
 		menu = new MenuStrip(start, options, credits, quit);
 		menu.setLocation(180, 200);
+		
+		//load font
+		titleFont = Utility.getFont(FONT_SIZE);
+		
+		//load background image
+		bg = new Image("res/tetripocalypse.png");
 	}
 	
-	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
-		g.drawImage(new Image("res/tetripocalypse.png"), 0, 0);
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
+			throws SlickException {
+		g.drawImage(bg, 0, 0);
 		menu.render(g);
+		//draw a nifty shadow
 		titleFont.drawString(100, 100, "TETRIPOCALYPSE", Color.black);
 		titleFont.drawString(100, 97, "TETRIPOCALYPSE", Color.orange);
 	}
 	
-	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+	public void update(GameContainer gc, StateBasedGame sbg, int delta)
+			throws SlickException {
 		Input input = gc.getInput();
 		int selection = menu.acceptInput(input);
 		switch (selection) {
@@ -52,7 +72,7 @@ public class Menu extends BasicGameState {
 				sbg.enterState(1);
 			break;
 		case 1:
-			//TODO: this stuff.
+			//TODO implement the options screen
 			//sbg.enterState(OPTION SCREEN);
 			break;
 		case 2:
@@ -63,22 +83,21 @@ public class Menu extends BasicGameState {
 			System.exit(0);
 			break;
 		}
-		
-		//int mouseXPos = Mouse.getX();
-		//int mouseYPos = Mouse.getY();
-		//mouse = "Mouse position x: " + mouseXPos + " y: " + mouseYPos;
-		//Input input = gc.getInput();
-		//if (input.isKeyDown(Input.KEY_UP)) { gordonY -= 4; }
-		//if (input.isKeyDown(Input.KEY_DOWN)) { gordonY += 4; }
-		//if (input.isKeyDown(Input.KEY_LEFT)) { gordonX -= 4; }
-		//if (input.isKeyDown(Input.KEY_RIGHT)) { gordonX += 4; }
 	}
 	
+	
+	/** Sets a reference to the game so we know whether
+	 *  to start a new game or to resume an existing one.
+	 *  @param p the game */
 	public void setPlay(Play p) {
 		play = p;
 	}
 	
+	/**
+	 * Returns the ID of this BasicGameState.
+	 * @return the ID of this BasicGameState
+	 */
 	public int getID() {
-		return 0; //yeah, look at the constructor..
+		return state;
 	}
 }
